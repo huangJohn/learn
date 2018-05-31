b1da52290e8986805a3eca23e4a045a2f00df8b1
CN\zhuanghuang <zhuanghuang@corp.netease.com>
2018-05-31 15:31:41 +0800

    dead lock

---
+++ src/main/java/concurrency/thread/dead_lock/DeadLock.java
@@ -0,0 +1,30 @@
+package concurrency.thread.dead_lock;
+
+/**
+ * @author zhuanghuang
+ * @date 2018/5/31
+ */
+
+public class DeadLock {
+
+    private OtherService otherService;
+
+    public DeadLock(OtherService otherService) {
+        this.otherService = otherService;
+    }
+
+    private static final Object LOCK = new Object();
+
+    public void d1() {
+        synchronized (LOCK) {
+            System.out.println("d1==========");
+            otherService.s1();
+        }
+    }
+
+    public void d2() {
+        synchronized (LOCK) {
+            System.out.println("d2=========");
+        }
+    }
+}
