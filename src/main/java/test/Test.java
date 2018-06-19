40424ffc4004dce828a3797447186138b9b82722
CN\zhuanghuang <zhuanghuang@corp.netease.com>
2018-06-19 16:26:22 +0800

    测试迭代器

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author zhuanghuang
  * @since 2018/5/31
@@ -8,5 +12,22 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("123");
+
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        for (Iterator<Integer> it = list.iterator(); it.hasNext(); ) {
+            Integer next = it.next();
+            System.out.println(next);
+            it.remove();
+        }
+
+        System.out.println("=======================");
+        list.forEach(s -> {
+            System.out.println(s);
+        });
     }
 }
