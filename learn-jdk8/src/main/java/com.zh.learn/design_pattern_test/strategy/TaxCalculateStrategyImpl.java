57b9622d9b395033afa6d023be8206d69dc5a59f
zhuanghuang <zhuanghuang@corp.netease.com>
2019-04-26 03:20:17 +0800

    add module

--- learn-jdk8/src/main/java/com.zh.learn/design_pattern_test/strategy/TaxCalculateStrategyImpl.java
+++
@@ -1,9 +0,0 @@
-package com.zh.learn.design_pattern_test.strategy;
-
-public class TaxCalculateStrategyImpl implements TaxCalculateStrategy {
-
-    @Override
-    public double calculateTax(double salary, double bonus) {
-        return salary * SALARY_RATE + bonus * BONUS_RATE;
-    }
-}
